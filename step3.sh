adfb2036159d76f427c02c7389249dd1e8695dc6
JamesBeeProg <JamesBeeProg@gmail.com>
2022-06-06 11:44:13 -0400

    Add more to step3

--- step3.sh
+++ step3.sh
@@ -2,7 +2,17 @@
 set -euo pipefail
 
 # Install yay
-sudo pacman -S --needed git base-devel
+sudo pacman -Syu --noconfirm git 
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si --noconfirm
+
+# Audio 
+yay -Syu --noconfirm pulseaudio
+pulseaudio --start
+
+# Login manager
+yay -Syu --noconfirm ly
+sudo systemctl enable ly
+
+echo Remember to reboot
